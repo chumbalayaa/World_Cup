25631821fd82c65a084f25eeaeb0b85b955e6ee8
Alex Chumbley <achumbley@ip-10-154-165-254.ec2.internal>
2014-06-02 13:51:17 +0000

    Trying out ESPN request

--- worldcup/views.py
+++ worldcup/views.py
@@ -1,6 +1,17 @@
 from flask import render_template, url_for, redirect
 from worldcup import app
+import unirest
+
+
+def getWorldCupInfo():
+    response = unirest.get("https://wmerydith-espn.p.mashape.com/sports/basketball/nba?apikey=%3Capikey%3E&_accept=application%2Fjson",
+        headers={
+            "X-Mashape-Authorization": "3gd92ZCjls37bbHhAZ1JmZXsDbYLOjiu"
+        }
+    )
+    return response
 
 @app.route('/')
 def index():
-    return "hello worldcup!"
+    json = getWorldCupInfo()
+    return render_template('index.html', json)
