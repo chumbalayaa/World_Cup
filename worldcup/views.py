799c1523f7bd1f230b2896695fee9530fa3d7310
Alex Chumbley <achumbley@ip-10-154-165-254.ec2.internal>
2014-06-02 14:20:53 +0000

    new params

--- worldcup/views.py
+++ worldcup/views.py
@@ -6,8 +6,10 @@ import unirest
 def getWorldCupInfo():
     response = unirest.get("https://wmerydith-espn.p.mashape.com/sports/basketball/nba?apikey=%3Capikey%3E&_accept=application%2Fjson",
         headers={
-            #"X-Mashape-Authorization": "3gd92ZCjls37bbHhAZ1JmZXsDbYLOjiu"
-            'api-key': 'gdkr4pnmhes2nf963mj5wxyd'
+            "X-Mashape-Authorization": "3gd92ZCjls37bbHhAZ1JmZXsDbYLOjiu"
+        },
+        params={
+            "apikey": "gdkr4pnmhes2nf963mj5wxyd"
         }
     )
     return response
