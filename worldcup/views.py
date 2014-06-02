3fd1ba7e17ada4c863426c6d2f2543ab4ad501e7
Alex Chumbley <achumbley@ip-10-154-165-254.ec2.internal>
2014-06-02 14:01:26 +0000

    Fixed rendering bug

--- worldcup/views.py
+++ worldcup/views.py
@@ -14,4 +14,4 @@ def getWorldCupInfo():
 @app.route('/')
 def index():
     json = getWorldCupInfo()
-    return render_template('index.html', json)
+    return render_template('index.html', json=json)
